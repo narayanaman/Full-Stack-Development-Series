9077e9370cd95bb514360310bf9799c97b994780
Aman Kushwaha <amankushwaha4753@gmail.com>
2026-06-29 09:03:26 +0530

    Simon Say's Game

---
+++ Simon Project/app.js
@@ -0,0 +1,84 @@
+let gameSeq=[];
+let userSeq=[];
+
+let btns=["red","green","yellow","purple"];
+
+let started =false;
+let level=0;
+
+let h2=document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("Game is started");
+        started=true;
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+}
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelUp (){
+    userSeq =[];
+    level++; 
+    h2.innerText=`Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gamSeq);
+    gameFlash(randBtn);  
+}
+
+function checkAns(idx){
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,1000);
+            
+        }
+    }
+    else{
+        h2.innerHTML=`Game Over ! Your score was <b> ${level}</b> <br>Press Any key to start.`;
+        document.querySelector("body").style.backColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor ="white";
+        })
+        reset();
+    }
+}
+
+function btnPress(){
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+
+    checkAns();
+}
+let allBtns=document.querySelectorAll(".btn");
+for(brn of btns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
